0466cd1df00640f00cee841767d854f992d1b24c
omar chamoun <omarchamoun@me.com>
2017-12-05 14:55:41 +0100

    fixing controller

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,27 @@
 class FlatsController < ApplicationController
+
+  def index
+    @flats = flats.all
+  end
+
+  def show
+
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
 end
