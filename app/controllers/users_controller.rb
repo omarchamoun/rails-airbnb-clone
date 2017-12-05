8063d8c6dae31837ddb945dd60b58abf91a2c625
Sergio Alves <salves2383@gmail.com>
2017-12-05 16:58:53 +0100

    Added Users controller and Flats controller small changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,38 @@
 class UsersController < ApplicationController
 
+  before_action :set_user
+
   def show
+
   end
 
   def edit
+
   end
 
   def update
+    @user = User.update(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @user.destroy
+    redirect_to flats_path
   end
 
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :gender)
+  end
 
 end
 
