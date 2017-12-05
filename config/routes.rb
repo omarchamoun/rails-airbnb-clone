cb9db1a7d4946e0b170bbbc591020bc4d62db080
omar chamoun <omarchamoun@me.com>
2017-12-05 17:51:39 +0100

    finish views flat

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'flats#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :registrations, :controllers => { registrations: "registrations"}
 
