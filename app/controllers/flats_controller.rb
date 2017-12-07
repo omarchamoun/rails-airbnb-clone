7e02cf3924003533f2f98c1c80e2509803ced6b6
Sergio Alves <salves2383@gmail.com>
2017-12-07 20:45:15 +0100

    Navbar is now fixed, appear everywhere apart from the home page. Added all the bookings logic and the view, users can now book a flat, and have a bookings page where they can see there bookings and if they are a host, they can see the bookings requests

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -4,14 +4,11 @@ before_action :set_flat, only: [:show, :edit, :update, :destroy]
 
   def index
     @flats = Flat.where.not(latitude: nil, longitude: nil)
-
     @markers = Gmaps4rails.build_markers(@flats) do |flat, marker|
     marker.lat flat.latitude
     marker.lng flat.longitude
     marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
-
     end
-
   end
 
   def show
