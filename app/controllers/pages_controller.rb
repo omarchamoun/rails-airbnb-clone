90d5eee432d1a97f02637ba9c3b4b1e640d3e66f
Martine Eisinga <eisingamartine@gmail.com>
2017-12-08 12:20:00 +0100

    design frontend

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
   def home
     @flats = Flat.all
+    @markers = Gmaps4rails.build_markers(@flats) do |flat, marker|
+        marker.lat flat.latitude
+        marker.lng flat.longitude
+        marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
+      end
   end
 end
